bf83eec8b25c275110a682d318d544eb8309692a
Denys Zariaiev <denys.zariaiev@gmail.com>
2014-04-08 15:04:48 +0300

    Working test case

--- test/index.js
+++ test/index.js
@@ -2,11 +2,21 @@ var EDBO = require('../');
 
 EDBO.setGlobalConfig('config.json');
 
+var currentSession;
+
 EDBO
   .session()
   .then(function(session) {
-    // TODO
-    console.log(session);
+    currentSession = session;
+    return currentSession.Person.PersonSexTypesGet();
+  })
+  .then(function(data) {
+    console.log(data.dPersonSexTypes);
+
+    return currentSession.Guides.BenefitsGet();
+  })
+  .then(function(data) {
+    console.log(data.dBenefits);
   })
   .catch(function(err) {
     console.log(err);
